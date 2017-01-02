85c4bab4a5cf5270ceb8229645183e43a22f9e13
Monsieur Tino <contact@mrtino.eu>
2017-01-02 15:53:40 +0100

    First commit

---
+++ candidats.js
@@ -0,0 +1,61 @@
+$('#amelenchon').click(function() {
+  $('#jeanlucnorevolution').show();
+  $('#lechristcosmique').hide();
+  $('#jenesuispassocialiste').hide();
+  $('#jeseraiaudeuxiemetour').hide();
+  $('#epilationdessourcils').hide();
+  $('#laracecongoide').hide();
+});
+
+$('#adurif').click(function() {
+  $('#jeanlucnorevolution').hide();
+  $('#lechristcosmique').show();
+  $('#jenesuispassocialiste').hide();
+  $('#jeseraiaudeuxiemetour').hide();
+  $('#epilationdessourcils').hide();
+  $('#laracecongoide').hide();
+});
+
+$('#amacron').click(function() {
+  $('#jeanlucnorevolution').hide();
+  $('#lechristcosmique').hide();
+  $('#jenesuispassocialiste').show();
+  $('#jeseraiaudeuxiemetour').hide();
+  $('#epilationdessourcils').hide();
+  $('#laracecongoide').hide();
+});
+
+$('#aaignan').click(function() {
+  $('#jeanlucnorevolution').hide();
+  $('#lechristcosmique').hide();
+  $('#jenesuispassocialiste').hide();
+  $('#jeseraiaudeuxiemetour').show();
+  $('#epilationdessourcils').hide();
+  $('#laracecongoide').hide();
+});
+
+$('#afillon').click(function() {
+  $('#jeanlucnorevolution').hide();
+  $('#lechristcosmique').hide();
+  $('#jenesuispassocialiste').hide();
+  $('#jeseraiaudeuxiemetour').hide();
+  $('#epilationdessourcils').show();
+  $('#laracecongoide').hide();
+});
+
+$('#alesquen').click(function() {
+  $('#jeanlucnorevolution').hide();
+  $('#lechristcosmique').hide();
+  $('#jenesuispassocialiste').hide();
+  $('#jeseraiaudeuxiemetour').hide();
+  $('#epilationdessourcils').hide();
+  $('#laracecongoide').show();
+});
+
+$('#ajadot').click(function() {
+    alert("Le programme de Yannick Jadot sera connu en janvier 2017. Reviens plus tard pour le découvrir !");
+});
+
+$('#alepen').click(function() {
+    alert("Le programme de Marine Le Pen sera connu en février 2017. Reviens plus tard pour le découvrir !");
+});
