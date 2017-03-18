e261c93e5cf46e811ba7db90321bc3cde4ddf69e
Monsieur Tino <contact@mrtino.eu>
2017-03-18 21:07:45 +0100

    Version finale (?)

--- img/images.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in *.jpg; do
- echo $i
- jpegtran -copy none -optimize -outfile temp_optimize.jpg $i
- jpegtran -copy none -progressive -outfile $i temp_optimize.jpg
- rm temp_optimize.jpg
-done
